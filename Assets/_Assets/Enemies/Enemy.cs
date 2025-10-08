8567a363a131e3beae84286b3f943dcdfed0646e
Almendarez <jdalmend@student.uiwtx.edu>
2025-10-08 16:34:02 -0500

    added enemy behavior

--- Assets/_Assets/Enemies/Enemy.cs
+++ Assets/_Assets/Enemies/Enemy.cs
@@ -1,22 +1,36 @@
+using Unity.Behavior;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    GameObject mTarget;
     GameObject Target
     {
-        get { return Target; }
+        get { return mTarget; }
         set
         {
-            Target = value;
+            if (Target == value)
+                return;
+
+            if (value == null)
+            {
+                mBehaviorGraphAgent.BlackboardReference.SetVariableValue("HasLastSeenPosition", true);
+                mBehaviorGraphAgent.BlackboardReference.SetVariableValue("TargetLastSeenPosition", mTarget.transform.position);
+            }
+            mTarget = value;
+            mBehaviorGraphAgent.BlackboardReference.SetVariableValue("Target", mTarget);
         }
     }
 
     [SerializeField] float mEyeHeight = 1.5f;
     [SerializeField] float mSightDistance = 5f;
     [SerializeField] float mViewAngle = 30f;
-    void Start()
+    [SerializeField] float mAlwaysAwareDistance = 1.5f;
+
+    BehaviorGraphAgent mBehaviorGraphAgent;
+    void Awake()
     {
-        
+        mBehaviorGraphAgent = GetComponent<BehaviorGraphAgent>();
     }
 
 
@@ -27,6 +41,7 @@ public class Enemy : MonoBehaviour
 
     private void UpdatePlayerPerception()
     {
+        Debug.Log($"Checking Percption");
         Player player = GameMode.MainGameMode.mPlayer;
         if (!player)
         {
@@ -40,6 +55,13 @@ public class Enemy : MonoBehaviour
             return;
         }
 
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= mAlwaysAwareDistance)
+        {
+            Target = player.gameObject;
+            return;
+        }
+
         Vector3 playerDir = (player.transform.position - transform.position).normalized;
         if (Vector3.Angle(playerDir, transform.forward) > mViewAngle)
         {
@@ -50,7 +72,7 @@ public class Enemy : MonoBehaviour
         Vector3 eyeViewPoint = transform.position + Vector3.up * mEyeHeight;
         if(Physics.Raycast(eyeViewPoint, playerDir, out RaycastHit hitInfo, mSightDistance))
         {
-            if (hitInfo.collider.gameObject != player)
+            if (hitInfo.collider.gameObject != player.gameObject)
             {
                 Target = null;
                 return;
@@ -64,11 +86,19 @@ public class Enemy : MonoBehaviour
     {
         Vector3 eyeViewPoint = transform.position + Vector3.up * mEyeHeight;
         Gizmos.DrawWireSphere(eyeViewPoint, mSightDistance);
+        Gizmos.DrawWireSphere(eyeViewPoint, mAlwaysAwareDistance);
 
         Vector3 leftLineDir = Quaternion.AngleAxis(mViewAngle, Vector3.up) * transform.forward;
         Vector3 rightLineDir = Quaternion.AngleAxis(-mViewAngle, Vector3.up) * transform.forward;
 
         Gizmos.DrawLine(eyeViewPoint, eyeViewPoint + leftLineDir * mSightDistance);
         Gizmos.DrawLine(eyeViewPoint, eyeViewPoint + rightLineDir * mSightDistance);
+
+        if (Target)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, Target.transform.position);
+            Gizmos.DrawWireSphere(Target.transform.position, 0.5f);
+        }
     }
 }
